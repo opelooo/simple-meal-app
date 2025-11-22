84bd6dc27b5469adfa213633d74e126a94b9f4df
opelooo <mathys.alberino@gmail.com>
2025-11-22 11:46:45 +0700

    Update [101] Adding Images Styling

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,7 +1,7 @@
 import { View, FlatList, StyleSheet } from "react-native";
 
-import { MEALS } from "../data/dummy-data";
 import MealItem from "../components/MealItem";
+import { MEALS } from "../data/dummy-data";
 
 function MealsOverviewScreen({ route }) {
   const catId = route.params.categoryId;
